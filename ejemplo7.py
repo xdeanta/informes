c1525d60567351f949d74a4e1b6efe28f6d2cbf6
dam2a <xdeantabelisario@danielcastelao.org>
2021-02-25 12:27:58 +0100

    ejemplo 7 para terminar y añadido ejemplo 8

--- ejemplo7.py
+++ ejemplo7.py
@@ -2,6 +2,8 @@ from reportlab.platypus import Table
 from reportlab.platypus import  SimpleDocTemplate
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.barcharts import VerticalBarChart
 
 datos = [
             ['Arriba/Izquierda', '', '02', '03','04'],
@@ -24,6 +26,29 @@ tabla.setStyle([('BACKGROUND',(1,1),(-1,-1), colors.aliceblue)])
 tabla.setStyle([('INNERGRID', (1,1), (-1,-1), 0.25, colors.darkgray)])
 
 doc = [tabla]
+
+d = Drawing(400,400)
+gb = VerticalBarChart()
+gb.x = 50
+gb.y = 50
+gb.height = 125
+gb.width = 300
+gb.data = listaDatosEdade
+gb.strokeColor = colors.black
+gb.valueAxis.valueMin = 0
+gb.valueAxis.valueMax = 100
+gb.valueAxis.valueStep = 10
+gb.categoryAxis.labels.boxAnchor = 'ne'
+gb.categoryAxis.labels.dx = 8
+gb.categoryAxis.labels.dy = -2
+gb.categoryAxis.labels.angle = 30
+gb.categoryAxis.categoryNames = listaDatosNome
+gb.groupSpacing = 10
+gb.barSpacing = 2
+
+d.add (gb)
+
+doc.append(d)
 
 documento = SimpleDocTemplate("informeTabla.pdf", pagesize = A4, showBoundary = 0)
 documento.build(doc)
---
+++ ejemplo8.py
@@ -0,0 +1,64 @@
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.legends import Legend
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate
+guion = []
+
+d = Drawing(300,200)
+tarta = Pie()
+tarta.x = 65
+tarta.y = 15
+tarta.width = 170
+tarta.height =170
+tarta.data = [10,20,30,40,50]
+tarta.labels = ['Brave', 'Opera', 'Firefox', 'Chrome', 'Edge']
+
+tarta.slices.strokeWidth = 0.5
+tarta.slices[3].popout = 10
+tarta.slices[3].strokeWidth = 2
+tarta.slices[3].strokeDashArray = [2,2]
+tarta.slices[3].labelRadius = 1.75
+tarta.slices[3].fontColor = colors.red
+
+tarta.sideLabels = 1
+
+leyenda = Legend()
+
+leyenda.x = 370
+leyenda.y = 0
+
+leyenda.dx = 8
+leyenda.dy = 8
+leyenda.fontName = 'Helvetica'
+leyenda.fontSize = 7
+leyenda.boxAnchor = 'n'
+leyenda.columnMaximum = 10
+leyenda.strokeWidth = 1
+leyenda.strokeColor = colors.darkgray
+leyenda.deltax = 75
+leyenda.deltay = 10
+leyenda.autoXPadding = 5
+leyenda.autoYPadding = 0
+leyenda.yGap = 0
+leyenda.dxTextSpace = 5
+leyenda.alignment= 'right'
+leyenda.dividerLines = 1|2|4
+leyenda.dividerOffsY=4.5
+leyenda.subCols.rpad = 30
+
+colores = [colors.blue, colors.red, colors.green, colors.yellow, colors.pink]
+for i, color in enumerate(colores):
+    tarta.slices[i].fillColor = color
+
+leyenda.colorNamePairs = [(tarta.slices[i].fillColor,
+                           (tarta.labels[i][0:20], '%0.2f' % tarta.data[i])
+                           ) for i in range(len(tarta.data))]
+
+d.add(tarta)
+d.add(leyenda)
+guion.append(d)
+
+doc = SimpleDocTemplate("exemploTarta.pdf", pagesize = A4)
+doc.build(guion)
